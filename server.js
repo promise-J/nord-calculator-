57d09471aaad28b76c9ba9d7a99c148e2ba2b733
Promise Johnson <chiemelapromise30@gmail.com>
2023-02-10 19:07:46 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/calculate", (req, res) => {
+  const { firstNumber, secondNumber, operation } = req.body;
+  if (!firstNumber || !secondNumber) return res.status(401).json("Please provide all fields");
+  try {
+    let result;
+    switch (operation) {
+      case "x":
+        result = firstNumber * secondNumber;
+        console.log(firstNumber * secondNumber)
+        break;
+      case "+":
+        console.log(firstNumber + secondNumber)
+        result = firstNumber + secondNumber;
+        break;
+      case "-":
+          console.log(firstNumber - secondNumber)
+          result = firstNumber - secondNumber;
+          break;
+      default:
+        result = 'Invalid input';
+        break;
+    }
+    return res.status(200).json(result);
+  } catch (error) {}
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER IS RUNNING ON ${PORT}`);
+});
